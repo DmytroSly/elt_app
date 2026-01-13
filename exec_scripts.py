586e0aae404a9988e342f2a9eaf6e52b6a9cec43
Dmytro Polishchuk <dpolishchuk@scalefree.com>
2026-01-13 08:43:21 +0100

    feat(exec_scripts): add exec_scripts

---
+++ exec_scripts.py
@@ -0,0 +1,52 @@
+import metadata
+from metadata import add_metadata_record, get_pipeline_id
+from importlib import reload
+
+import dlt
+from dlt.destinations import snowflake
+from dlt.sources.sql_database import sql_database, sql_table, Table
+from dlt.sources.credentials import ConnectionStringCredentials
+
+import sqlite3
+import json
+
+
+reload(metadata)
+
+dataset_record = dict(
+    pipeline_id=metadata.get_pipeline_id('test_pipeline'),
+    source_database='db_source_for_dlt',
+    source_schema='public',
+    source_table='employees',
+    destination_database='hub_speak_dmytro_base',
+    destination_schema='postgres_data',
+    destination_table='employees',
+    incremental_column='updated'
+)
+
+metadata.add_metadata_record(
+    table_name='dataset',
+    insert_values=dataset_record
+)
+
+
+# TODO next steps:
+# -+ Add a pipeline postgres - Snowflake
+#   -+ Try loading tables
+#   -+ Loading standalone table
+#   - Loading a set of tables
+# - Add a pipeline filesystem - Snowflake
+#   - Try loading tables
+# - Start working on CLI to add connections, pipelines, and datasets
+#   - Accepting user input with input()  
+
+# Needed skills:
+    # - error handling in Python.
+        # Read errors from database and return human readable message
+    # - Modules
+    # - REPL
+    
+# Run IDLE from a Python virtual environment
+# Start the virtual environment
+# Run python -m idlelib.idle
+# https://stackoverflow.com/questions/4924068/how-to-launch-python-idle-from-a-virtual-environment-virtualenv
