438bf17e9876bedc0ec6f310a0f41d850edc3ca6
Dmytro Polishchuk <dpolishchuk@scalefree.com>
2026-01-13 08:40:14 +0100

    feat(encryption): add encryption

---
+++ encryption.py
@@ -0,0 +1,21 @@
+import os
+from cryptography.fernet import Fernet
+
+
+def get_encryption_key():
+    key_file_path = os.getenv('KEY_FILE_PATH')
+    if os.path.exists(key_file_path):
+        with open(key_file_path, 'rb') as f:
+            return f.read()
+    else:
+        key = Fernet.generate_key()
+        with open(key_file_path, 'wb') as f:
+            f.write(key)
+        return key
+
+ENCRYPTION_KEY = get_encryption_key()
+cipher = Fernet(ENCRYPTION_KEY)
+
+if __name__ == '__main__':
+    encrypted_pass = cipher.encrypt(b'apspp')
+    print(encrypted_pass)
